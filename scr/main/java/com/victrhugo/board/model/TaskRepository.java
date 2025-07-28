8f2c9be37208469dc4218cafc746c4e22c9a831e
Victor Hugo <163581915+victrhugo@users.noreply.github.com>
2025-07-28 17:19:08 -0300

    Delete scr/main/java/com/victrhugo/board/model/TaskRepository.java

--- scr/main/java/com/victrhugo/board/model/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.victhugo.board.repository;
-
-import com.victhugo.board.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
