d98c1c26debacd11abc9c295e7908228f66e5466
Victor Hugo <163581915+victrhugo@users.noreply.github.com>
2025-07-28 17:18:34 -0300

    Create Task.java

---
+++ scr/main/java/com/victrhugo/board/model/Task.java
@@ -0,0 +1,21 @@
+package com.victhugo.board.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+}
